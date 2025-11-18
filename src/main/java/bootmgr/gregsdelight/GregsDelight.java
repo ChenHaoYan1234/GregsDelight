b0fbf058f0fc8a7a50ac6c99d391333747fce091
ChenHaoYan1234 <chanhaoyan@126.com>
2025-11-18 12:44:35 +0800

    Rename and rebrand mod to Greg's Delight

--- src/main/java/bootmgr/gregsdelight/GregsDelight.java
+++ src/main/java/bootmgr/gregsdelight/GregsDelight.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package bootmgr.gregsdelight;
 
 import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.data.chemical.material.event.MaterialEvent;
@@ -22,15 +22,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(GregsDelight.MOD_ID)
 @SuppressWarnings("removal")
-public class ExampleMod {
+public class GregsDelight {
 
-    public static final String MOD_ID = "examplemod";
+    public static final String MOD_ID = "gregsdelight";
     public static final Logger LOGGER = LogManager.getLogger();
-    public static GTRegistrate EXAMPLE_REGISTRATE = GTRegistrate.create(ExampleMod.MOD_ID);
+    public static GTRegistrate EXAMPLE_REGISTRATE = GTRegistrate.create(GregsDelight.MOD_ID);
 
-    public ExampleMod() {
+    public GregsDelight() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
@@ -81,7 +81,7 @@ public class ExampleMod {
      * @param event
      */
     private void addMaterialRegistries(MaterialRegistryEvent event) {
-        GTCEuAPI.materialManager.createRegistry(ExampleMod.MOD_ID);
+        GTCEuAPI.materialManager.createRegistry(GregsDelight.MOD_ID);
     }
 
     /**
